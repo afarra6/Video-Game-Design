d464011d06b1758c2eb796f77de3812d62d7c517
afarra6 <133391238+afarra6@users.noreply.github.com>
2023-05-12 11:51:28 -0500

    Add files via upload

---
+++ Coding Assignments/Variables/Variables.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+
+#1: Create a new variable and give it a default value of 10
+
+
+
+
+#2: Create a new exported variable and give it the default string "hello world!"
+
+
+
+#3: Create a new typed variable that is a float with the value 20.6
+
+
+
+
+#4: Create a new onready variable with the default boolean false
+
+
+
+
+
+#5: Inside the function below, create a local variable with the default string "local" 
+# and print all of your variables to the console
+func _ready():
+	
+	
+	
+	
+	
+	
+	pass
