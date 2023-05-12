a7af327f72c07add41f822d353f90bfd7214f6b8
afarra6 <afarra6@lsu.edu>
2023-05-12 12:49:23 -0500

    Coding Assignments

---
+++ Coding Assignments Project/Process Function/Process Function.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+#1: Create a new export variable with a default value of 0
+
+#2: Create a new variable with a default value of 200
+
+#Here is the process function. This function is ran every frame
+#the delta is the time elapsed in seconds between each frame
+
+func _process(delta):
+	#3: add a line of code here that prints the export variable to the console
+	
+	
+	#3: below, increase the exported variable by 1 each frame, 
+	# run the scene then then take a screenshot of your console and paste it in a google doc
+	
+	
+	#4: Delete your previous code and add a new line below that decreases the export variable by 1 each frame
+	#  run the scene then take a screenshot of your console and paste it in the same google doc
+	
+	
+	#5: Delete your previous code and add a new line of code that increases the export variable by the non-export
+	# varible each second. Run the scene then take a screenshot of your console and paste it in the same google doc
+	# (hint: you will need to use delta in some way)
+	
+	
+	
+	
+	#6: Delete your previous code and add a new line of code that decreases the export variable by the non-export
+	# varible each second. Run the scene then take a screenshot of your console and paste it in the same google doc
+	# (hint: you will need to use delta in some way)
+	
+	
+	
+	pass
