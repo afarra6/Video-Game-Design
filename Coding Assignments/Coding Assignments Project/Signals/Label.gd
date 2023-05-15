4fa09981458cd7cc2ce5c615eeb2d24092d41e73
afarra6 <afarra6@lsu.edu>
2023-05-15 17:11:54 -0500

    Added signals

---
+++ Coding Assignments/Coding Assignments Project/Signals/Label.gd
@@ -0,0 +1,14 @@
+extends Label
+
+
+var current_text = 0
+
+
+
+func _process(delta):
+	
+	text = str(current_text)
+	
+
+
+#6: In the connected signal, set the variable current_text equal to the passed argument
---
+++ Coding Assignments/Coding Assignments Project/Signals/signals.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+#Signals are a very powerful tool that allows us to communicate between nodes and scripts.
+#Nodes have a number of built in Signals, but we can define our own Signals as well
+
+#1: Create a new variable below, give it the default value of 0
+
+
+#2: Create a new signal below
+
+#3: Connect the signal to the Label
+
+func _process(delta):
+	
+	#4 Emit our signal below, pass our variable as an argument for the signal
+	
+	
+	
+	pass
+
+
+#5: Connect Timer1's timeout() signal to this node
+# Inside of the connected signal's function, increment our variable by 10
