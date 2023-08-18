ae0e009e6a7c6f89741b4bb07b148af84a3e087e
afarra6 <afarra6@lsu.edu>
2023-08-18 08:34:42 -0500

    Update Variables.gd

--- Coding Assignments/Coding Assignments Project/Variables/Variables.gd
+++ Coding Assignments/Coding Assignments Project/Variables/Variables.gd
@@ -3,24 +3,18 @@ extends Node2D
 
 #1: Create a new variable and give it a default value of 10
 
-var my_number = 10
 
 
 #2: Create a new exported variable and give it the default string "hello world!"
 
-@export var my_second_number = 'hello world!'
 
 
 #3: Create a new typed variable that is a float with the value 20.6
-
-var my_float := "20.6"
 
 
 
 
 #4: Create a new onready variable with the default boolean false
-
-@onready var my_boolean = false
 
 
 #Below is the ready function. This function runs one time when 
@@ -38,9 +32,8 @@ func _ready():
 	# for this assignment
 	
 	
-	print("hi")
+	
 	
 	pass
-
 
 
